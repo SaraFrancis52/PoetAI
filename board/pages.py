bd835502fdb03130162c3294ee57e3738c244061
Sara Francis <saradf.paz@gmail.com>
2024-05-01 12:38:04 -0400

    Added loading screen

--- board/pages.py
+++ board/pages.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, render_template, request
 from ai import *
 
+# Fixme: no global variable?
+data = []
+
 bp = Blueprint("pages", __name__)
 
 @bp.route("/")
@@ -15,8 +18,14 @@ def about():
 def generatepoem():
     return render_template("pages/generatepoem.html")
 
-@bp.route("/poem")
-def poem():
+@bp.route("/load")
+def load():
+    subject = request.args.get('subject')
+    print("(Loading) You requested " + subject)
+    return render_template("pages/load.html", topic=subject)
+
+@bp.route("/getResponse")
+def getResponse():
     subject = request.args.get('subject')
     print("You requested: " + subject)
     myData = []
@@ -34,7 +43,13 @@ def poem():
     print(theResponse)
     parsedData = parse_data(theResponse[0])
     print(parsedData)
-    return render_template("pages/poem.html", aiResponse=parsedData)
+    global data 
+    data = parsedData
+    return "done"
+    
+@bp.route("/poem")
+def poem():
+    return render_template("pages/poem.html", aiResponse=data)
 
 @bp.route("/savedpoems")
 def savedpoems():
