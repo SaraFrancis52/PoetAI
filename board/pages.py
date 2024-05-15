f6795c4b48bd44ba0690d8c12af8525eff86ac65
Sara Francis <saradf.paz@gmail.com>
2024-05-15 15:46:10 -0400

    Added saving poems and viewing them from the db

--- board/pages.py
+++ board/pages.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from ai import *
+import sqlite3
 
 # Fixme: no global variable?
 data = []
@@ -54,5 +55,49 @@ def poem():
 @bp.route("/savedpoems/<pid>")
 def savedpoems(pid):
     print("You'd like id " + pid)
-    dbData=data
-    return render_template('pages/savedpoems.html', pid=pid)
+    conn = sqlite3.connect('poetAI.db')
+    cursor = conn.cursor()
+    cursor.execute("SELECT rating, poem from poems where id = " + pid)
+    output = cursor.fetchall() 
+    conn.close()
+    for row in output: 
+        print(row) 
+        rating = row[0]
+        text = row[1]
+    print("The rating is " + str(rating))
+    parsedData = parse_data(text)
+    print(parsedData)
+    pidA = int(pid)-1
+    pidB = int(pid)
+    pidC = int(pid)+1
+    pidD = int(pid)+2
+    return render_template('pages/savedpoems.html', pid=pid, pidA=pidA, pidB=pidB, pidC=pidC, pidD=pidD, rating=rating, poem=parsedData)
+
+@bp.route('/saving_poem_background')
+def saving_poem_background():
+    rating = request.args.get('rating')
+    print(rating)
+    global data
+    toclean = data
+    print(toclean)
+    toclean = clean_data(toclean)
+    print(toclean)
+
+    conn = sqlite3.connect('poetAI.db')
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO poems (poem, rating) VALUES ('"+ toclean +"', " + rating + ")")
+    conn.commit()
+    conn.close()
+    return ("nothing")
+
+
+# Turn dictionary into a string for saving, and remove ' to avoid SQL parsing errors
+# FIXME how to save data with single quotes?
+def clean_data(data) :
+    cleaned = ''
+    cleaned.replace("'", "")
+    for row in data:
+        cleaned += row["text"]
+        cleaned += '\n'
+    cleaned = cleaned.replace("'", "")
+    return cleaned
